82ed49b390d280d9e20a2caab668b1fb8a997b4c
Munotasir2762 <muntasirpranto2762@gmail.com>
2024-01-22 23:51:45 +0600

    Javascript function

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+function sum (){
+    event.preventDefault();
+    let number1 = document.getElementById('number1').value;  //43
+    let number2 = document.getElementById('number2').value;  //NaN
+
+    if(number1 == ''){
+        number1 = 0;
+    }
+
+    if(number2 == ''){
+        number2 = 0;
+    }
+
+    let numberFloat1 = parseFloat(number1); 
+    let numberFloat2 = parseFloat(number2);
+
+    let result = numberFloat1 + numberFloat2;
+
+    console.log(result);
+    document.getElementById('result').innerHTML = 'Result is= ' + result;
+}
